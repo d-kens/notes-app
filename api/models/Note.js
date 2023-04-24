371fde25f5693db07c6ece1ddcfe890b5683198a
d-kens <omondikens1@gmail.com>
2023-04-24 15:35:40 +0300

    removed the sequence plugin when creating a new note

--- api/models/Note.js
+++ api/models/Note.js
@@ -1,5 +1,4 @@
-const mongoose = require('mongoose')
-const AutoIncrement = require('mongoose-sequence')(mongoose)
+const mongoose = require('mongoose');
 
 const noteSchema = new mongoose.Schema(
     {
@@ -24,12 +23,6 @@ const noteSchema = new mongoose.Schema(
     {
         timestamps: true
     }
-)
-// TODO: Look into mongoose doc to fix the issue
-// noteSchema.plugin(AutoIncrement, {
-//     inc_field: 'ticket',
-//     id: 'ticketNums',
-//     start_seq: 500
-// })
+);
 
 module.exports = mongoose.model('Note', noteSchema)
